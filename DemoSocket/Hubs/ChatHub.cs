86535c2825e2ad99025daf51ab0d0115638060ad
Khun <khun.ly@bstorm.be>
2024-11-07 09:47:06 +0100

    Add project files.

---
+++ DemoSocket/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DemoSocket.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void NewMessage(string message)
+        {
+
+        }
+    }
+}
---
+++ DemoSocket/Program.cs
@@ -0,0 +1,30 @@
+using DemoSocket.Hubs;
+using Microsoft.AspNetCore.Http.Connections;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/ws/chat", o => o.Transports = HttpTransportType.WebSockets);
+
+app.Run();
